eb5589041559d5100218ebf72ed9e64aa619f21e
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-01-06 16:46:09 +0000

    v9.2.3 (#3)
    Fixes #2

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Run Comment Mop using the three-dot menu or mod shield menu (depending on platfo
 
 From version 9.2, you can configure the default options for the Mop form in your app settings, which you can find via the [Developer Platform app portal](https://developers.reddit.com/my/communities).
 
+Mopping extremely large chains can fail, if this happens please try again, ensuring that the option to skip already actioned comments is enabled.
+
 ## Credits
 
 This app was originally written by /u/FlyingLaserTurtle, with contributions from /u/ni5arga.
@@ -12,9 +14,13 @@ For support for Comment Mop, please contact /u/fsv (the current maintainer) rath
 
 ## Source code and license
 
-Comment Mop is open source and licensed under the BSD Three Clause license. The source code is available on GitHub [here](https://github.com/fsvreddit/comment-nuke).
+Comment Mop is open source and licensed under the BSD Three Clause license. [The source code is available on GitHub here](https://github.com/fsvreddit/comment-nuke).
 
 ## Change History
+
+### v9.2.3
+
+* More reliable mopping of extremely large threads
 
 ### v9.2.0
 
--- src/nuke.ts
+++ src/nuke.ts
@@ -1,5 +1,4 @@
 import { Comment, Context, FormFunction, FormOnSubmitEvent, JSONObject, Post } from "@devvit/public-api";
-import { chunk } from "lodash";
 import pluralize from "pluralize";
 
 export enum NukeFormField {
@@ -51,65 +50,45 @@ export interface NukeProps {
     target: Post | Comment;
 }
 
-async function getAllCommentsInThread (comment: Comment, skipDistinguished: boolean): Promise<Comment[]> {
-    const comments: Comment[] = [];
+interface ActionResult {
+    promises: Promise<void>[];
+    commentsActioned: number;
+}
 
-    if (!skipDistinguished || !comment.isDistinguished()) {
-        comments.push(comment);
+async function actionAllCommentsInThread (comment: Comment, nukeProps: NukeProps): Promise<ActionResult> {
+    const promises: Promise<void>[] = [];
+    let actioned = false;
+
+    if (nukeProps.remove && (!nukeProps.skipDistinguished || !comment.isDistinguished()) && (!nukeProps.skipAlreadyActioned || !comment.removed)) {
+        promises.push(comment.remove());
+        actioned = true;
+    }
+
+    if (nukeProps.lock && (!nukeProps.skipAlreadyActioned || !comment.locked)) {
+        promises.push(comment.lock());
+        actioned = true;
     }
 
     const replies = await comment.replies.all();
-    const replyResults = await Promise.all(replies.map(reply => getAllCommentsInThread(reply, skipDistinguished)));
+    const actions = await Promise.all(replies.map(reply => actionAllCommentsInThread(reply, nukeProps)));
 
-    comments.push(...replyResults.flat());
-
-    return comments;
+    return {
+        promises: [...promises, ...actions.map(action => action.promises).flat()],
+        commentsActioned: (actioned ? 1 : 0) + actions.reduce((sum, action) => sum + action.commentsActioned, 0),
+    };
 }
 
-async function getAllCommentsInPost (post: Post, skipDistinguished: boolean): Promise<Comment[]> {
-    const comments: Comment[] = [];
-
+async function actionAllCommentsInPost (post: Post, nukeProps: NukeProps) {
     const replies = await post.comments.all();
-    const replyResults = await Promise.all(replies.map(reply => getAllCommentsInThread(reply, skipDistinguished)));
+    const actions = await Promise.all(replies.map(reply => actionAllCommentsInThread(reply, nukeProps)));
 
-    comments.push(...replyResults.flat());
-
-    return comments;
-}
-
-async function nukeComments (comments: Comment[], shouldLock: boolean, shouldRemove: boolean, skipAlreadyActioned: boolean): Promise<boolean> {
-    try {
-        // Chunk comments into 30 items at a a time to reduce the risk of failure.
-        const commentChunks = chunk(comments, 30);
-
-        for (const chunk of commentChunks) {
-            if (shouldRemove) {
-                if (skipAlreadyActioned) {
-                    await Promise.all(chunk.filter(comment => !comment.removed).map(comment => comment.remove()));
-                } else {
-                    await Promise.all(chunk.map(comment => comment.remove()));
-                }
-            }
-
-            if (shouldLock) {
-                if (skipAlreadyActioned) {
-                    await Promise.all(chunk.filter(comment => !comment.locked).map(comment => comment.lock()));
-                } else {
-                    await Promise.all(chunk.map(comment => comment.lock()));
-                }
-            }
-        }
-
-        return true;
-    } catch (error) {
-        console.error("Failed to nuke comments:", error);
-        return false;
-    }
+    return {
+        promises: actions.map(action => action.promises).flat(),
+        commentsActioned: actions.reduce((sum, action) => sum + action.commentsActioned, 0),
+    };
 }
 
 export async function handleNukePostForm (event: FormOnSubmitEvent<JSONObject>, context: Context) {
-    const { values } = event;
-
     if (!context.postId) {
         console.error("No post ID");
         throw new Error("No post ID");
@@ -118,15 +97,25 @@ export async function handleNukePostForm (event: FormOnSubmitEvent<JSONObject>, 
     const target = await context.reddit.getPostById(context.postId);
 
     const nukeProps: NukeProps = {
-        remove: values.remove as boolean,
-        lock: values.lock as boolean,
-        skipDistinguished: values.skipDistinguished as boolean,
-        skipAlreadyActioned: values.skipAlreadyActioned as boolean,
+        remove: event.values.remove as boolean,
+        lock: event.values.lock as boolean,
+        skipDistinguished: event.values.skipDistinguished as boolean,
+        skipAlreadyActioned: event.values.skipAlreadyActioned as boolean,
         target,
     };
 
     if (!nukeProps.lock && !nukeProps.remove) {
         context.ui.showToast("You must select either lock or remove.");
+        return;
+    }
+
+    if (nukeProps.lock && !nukeProps.remove) {
+        if (target.locked) {
+            context.ui.showToast("The post is already locked. Locking individual comments is not necessary.");
+        } else {
+            await target.lock();
+            context.ui.showToast("Rather than locking individual comments, the post has been locked.");
+        }
         return;
     }
 
@@ -157,25 +146,25 @@ export async function handleNukeCommentForm (event: FormOnSubmitEvent<JSONObject
 async function handleNuke (nukeProps: NukeProps, context: Context): Promise<void> {
     const start = Date.now();
     try {
-        let comments: Comment[];
+        let actionResult: ActionResult;
 
         if (nukeProps.target instanceof Comment) {
-            comments = await getAllCommentsInThread(nukeProps.target, nukeProps.skipDistinguished);
+            actionResult = await actionAllCommentsInThread(nukeProps.target, nukeProps);
         } else {
-            comments = await getAllCommentsInPost(nukeProps.target, nukeProps.skipDistinguished);
+            actionResult = await actionAllCommentsInPost(nukeProps.target, nukeProps);
         }
 
         const commentGatherEnd = Date.now();
-        console.log(`Gathered ${comments.length} ${pluralize("comment", comments.length)} in ${commentGatherEnd - start}ms`);
-
-        if (comments.length === 0) {
+        console.log(`${nukeProps.target.id}: Gathered ${actionResult.promises.length} ${pluralize("promises", actionResult.promises.length)} in ${commentGatherEnd - start}ms`);
+        if (actionResult.promises.length === 0) {
             console.log(`${nukeProps.target.id}: No comments found to mop.`);
             context.ui.showToast("No comments found to mop.");
             return;
         }
 
-        const nukeResult = await nukeComments(comments, nukeProps.lock, nukeProps.remove, nukeProps.skipAlreadyActioned);
-        if (!nukeResult) {
+        const results = await Promise.allSettled(actionResult.promises);
+
+        if (results.some(result => result.status === "rejected")) {
             context.ui.showToast("Mop failed! Please try again later.");
             return;
         }
@@ -194,7 +183,7 @@ async function handleNuke (nukeProps: NukeProps, context: Context): Promise<void
             logVerbage = nukeProps.lock ? "lock" : "remove";
         }
 
-        console.log(`${nukeProps.target.id}: /u/${currentUsername} successfully ${toastVerbage} ${comments.length} ${pluralize("comment", comments.length)} in ${nukeEnd - commentGatherEnd}ms.`);
+        console.log(`${nukeProps.target.id}: /u/${currentUsername} successfully ${toastVerbage} ${actionResult.commentsActioned} ${pluralize("comment", actionResult.commentsActioned)} in ${nukeEnd - commentGatherEnd}ms.`);
 
         if (nukeProps.remove) {
             try {
